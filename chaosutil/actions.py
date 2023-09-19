9e6f90a8f67dfc41ab1f8e56b27d26a0676584eb
Mattias Cockburn <mattias.cockburn@posteo.net>
2023-09-19 11:59:06 +0200

    Initial commit

---
+++ chaosutil/actions.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from chaoslib.types import Configuration, Secrets
+from chaoslib.exceptions import ActivityFailed
+from jinja2 import Template, TemplateError
+from logzero import logger
+
+__all__ = ["template_file"]
+
+
+def template_file(
+    source: str,
+    destination: str,
+    context: dict = {},
+) -> None:
+    tpl = None
+    try:
+        with open(source) as i:
+            tpl = Template(i.read())
+    except OSError:
+        raise ActivityFailed(f"Cannot read input file {source}")
+
+    try:
+        with open(destination, 'w+') as out:
+            out.write(tpl.render(**context))
+    except OSError:
+        raise ActivityFailed(f"Cannot write output file {destination}")
+    except TemplateError:
+        raise ActivityFailed(f"Failed to template file {source}")
